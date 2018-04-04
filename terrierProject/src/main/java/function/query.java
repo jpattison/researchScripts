fe4de98bec9d8e6eec850ce7180cd42c3177d1e4
Jeremy Pattison <jeremypattison@Jeremys-MacBook-Pro.local>
2018-04-04 21:35:51 +1000

    updated java terrier, now associates filename with index

--- terrierProject/src/main/java/function/query.java
+++ terrierProject/src/main/java/function/query.java
@@ -1,30 +1,61 @@
 package function;
 
 import org.terrier.indexing.Collection;
+import org.terrier.indexing.SimpleFileCollection;
 import org.terrier.indexing.TRECCollection;
+import org.terrier.matching.ResultSet;
 import org.terrier.querying.Manager;
 import org.terrier.querying.QueryExpansion;
 import org.terrier.querying.SearchRequest;
 import org.terrier.structures.Index;
 import org.terrier.structures.IndexOnDisk;
+import org.terrier.structures.MetaIndex;
 import org.terrier.structures.indexing.Indexer;
 import org.terrier.structures.indexing.classical.BasicIndexer;
+import org.terrier.utility.ApplicationSetup;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class query {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.setProperty("terrier.home", "/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/");
 
         //Collection coll = new TRECCollection("C:\\Users\\Jeremy\\Documents\\ResearchProjectData\\house_hansard\\2014");
         // NOte note entirely sure about above, i dont think we're really using it properly
-        Indexer indexer = new BasicIndexer("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/index", "data");
-        //indexer.index(new Collection[]{ coll });
-        Index index = IndexOnDisk.createIndex("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/index", "data");
+//        Indexer indexer = new BasicIndexer("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/index", "data");
+//        Index index = IndexOnDisk.createIndex("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/index", "data");
+//        System.out.println("We have indexed " + index.getCollectionStatistics().getNumberOfDocuments() + " documents");
+
+
+        // Directory containing files to index
+        String aDirectoryToIndex = "/Users/jeremypattison/LargeDocument/ResearchProjectData/house_hansard/output/";
+
+        // Configure Terrier
+        ApplicationSetup.setProperty("indexer.meta.forward.keys", "filename");
+        ApplicationSetup.setProperty("indexer.meta.forward.keylens", "200");
+
+        Indexer indexer = new BasicIndexer("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/autoIndex", "data");
+        //Collection coll = new SimpleFileCollection(Arrays.asList(aDirectoryToIndex), true);
+
+        Collection coll = new TRECCollection(); //Arrays.asList(aDirectoryToIndex), true);
+        indexer.index(new Collection[]{coll});
+        //indexer.close();
+
+        Index index = Index.createIndex("/Users/jeremypattison/LargeDocument/ResearchProjectData/terrier-core-4.2/var/autoIndex", "data");
+
+        MetaIndex metaIndex = index.getMetaIndex();
+
+
         System.out.println("We have indexed " + index.getCollectionStatistics().getNumberOfDocuments() + " documents");
+
+
+
+
         Manager queryingManager = new Manager(index);
         SearchRequest srq = queryingManager.newSearchRequestFromQuery("austerity");
         srq.addMatchingModel("Matching", "BM25");
@@ -42,12 +73,41 @@ public class query {
 
 
 
-//		queryingManager.runSearchRequest(srq);
-//		ResultSet results = srq.getResultSet();
-//		String[] displayKeys = results.getMetaKeys();
+		queryingManager.runSearchRequest(srq);
+		ResultSet results = srq.getResultSet();
+		int[] docIds = results.getDocids();
 
-        //System.out.print(qexpan.lastExpandedQuery);
+		System.out.println("printing stuff");
 
+		System.out.println("We got {0} results".format(String.valueOf(results.getExactResultSize())));
+
+//		for (int temp : docIds) {
+//            System.out.println(String.valueOf(temp));
+//
+//
+//        }
+
+
+        // Print the results
+        System.out.println(results.getExactResultSize()+" documents were scored");
+        System.out.println("the following meta keys were identified");
+
+        String[] displayKeys = metaIndex.getKeys();
+
+            for (String temp : displayKeys) {
+                System.out.println(temp);
+
+        }
+
+
+
+        System.out.println("The top "+results.getResultSize()+" of those documents were returned");
+        System.out.println("Document Ranking");
+        for (int i =0; i< results.getResultSize(); i++) {
+            int docid = results.getDocids()[i];
+            double score = results.getScores()[i];
+            System.out.println("   Rank "+i+": "+docid+" "+metaIndex.getItem("filename", docid)+" "+score);
+        }
 
     }
 
