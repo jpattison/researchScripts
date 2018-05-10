f2bb397e29a20a92ac785299d0e0f4d2dadab284
Jeremy Pattison <pattisonjeremy@gmail.com>
2018-05-10 17:22:48 +1000

    presented new way of collecting budgets for hansard. Inlcuding budget estimates

--- dataAggregating/hansardHandler.py
+++ dataAggregating/hansardHandler.py
@@ -16,7 +16,7 @@ import os
 
 budget2017 = ["2017-05-09.json","2017-05-10.json","2017-05-11.json"]
 
-budget2016 = ["2016-05-02.json", "2016-05-03.json", "2016-05-04.json", "2016-05-05.json"]
+budget2016 = ["2016-05-03.json", "2016-05-04.json", "2016-05-05.json"]
 
 budget2015 = ["2015-05-12.json","2015-05-13.json","2015-05-14.json"]
 
@@ -26,7 +26,7 @@ budget2013 = ["2013-05-14.json","2013-05-15.json","2013-05-16.json"]
 
 budget2012 = ["2012-05-08.json", "2012-05-09.json", "2012-05-10.json"]
 
-budget2011 = ["2011-05-10.json", "2011-05-11.json", "2011-05-10.json", "2011-05-12.json"]
+budget2011 = ["2011-05-10.json", "2011-05-11.json", "2011-05-12.json"]
 
 budget2010 = ["2010-05-11.json", "2010-05-12.json", "2010-05-13.json"]
 
@@ -53,6 +53,63 @@ budgetList = [(budget2005, 2005), (budget2006, 2006),
 (budget2014, 2014), (budget2015, 2015), (budget2016, 2016),
 (budget2017, 2017)]
 
+politicalCalandar = {
+    
+    2005: {
+        "budget": [date(2005, 05, 10), date(2005, 05, 12)],
+        "estimates" : [date(2005, 05, 23), date(2005, 06, 03)]
+    },
+    2006: {
+        "budget": [date(2006, 05, 9), date(2006, 05, 11)],
+        "estimates" : [date(2006, 05, 22), date(2006, 06, 01)]
+    },
+    2007: {
+        "budget": [date(2007, 05, 8), date(2007, 05, 10)],
+        "estimates" : [date(2007, 05, 21), date(2007, 05, 31)]
+    },
+    2008: {
+        "budget": [date(2008, 05, 13), date(2008, 05, 15)],
+        "estimates" : [date(2008, 05, 26), date(2008, 06, 05)]
+    },    
+    2009: {
+        "budget": [date(2009, 05, 12), date(2009, 05, 14)],
+        "estimates" : [date(2009, 05, 25), date(2009, 06, 04)]
+    },
+    2010: {
+        "budget": [date(2010, 05, 11), date(2010, 05, 13)],
+        "estimates" : [date(2010, 05, 24), date(2010, 06, 03)]
+    },
+    2011: {
+        "budget": [date(2011, 05, 10), date(2011, 05, 12)],
+        "estimates" : [date(2011, 05, 23), date(2011, 06, 02)]
+    },
+    2012: {
+        "budget": [date(2012, 05, 8), date(2012, 05, 10)],
+        "estimates" : [date(2012, 05, 21), date(2012, 05, 31)]
+    },
+    2013: {
+        "budget": [date(2013, 05, 14), date(2013, 05, 16)],
+        "estimates" : [date(2013, 05, 27), date(2013, 06, 06)]
+    },    
+    2014: {
+        "budget": [date(2014, 05, 13), date(2014, 05, 15)],
+        "estimates" : [date(2014, 05, 26), date(2014, 06, 05)]
+    },
+    2015: {
+        "budget": [date(2015, 05, 12), date(2015, 05, 14)],
+        "estimates" : [date(2015, 05, 25), date(2015, 06, 04)]
+    },
+
+    2016: {
+        "budget": [date(2016, 05, 03), date(2016, 05, 04)],
+        "estimates" : [date(2016, 05, 05), date(2016, 05, 06)]
+    },
+    2017: {
+        "budget": [date(2017, 05, 9), date(2017, 05, 11)],
+        "estimates" : [date(2017, 05, 22), date(2017, 06, 01)]
+    }               
+}
+
 # I suppose the basic would be convert an entire year into single bag of words and use that
 #def getHansard(initialYear, finalYear, queryYear, source=bowDirectory)
 
@@ -73,6 +130,22 @@ def withinDates(initialDate, finalDate, bowDirectory = "/Users/jeremypattison/La
         if os.path.isfile(filepath):
             output.append(filename)
 
+    return output
+
+def getBudgets(initYear, finYear, budgetSession = False, budgetEstimates = False, skipFirstDay = False):
+    #skip first days infers skip budget day as budget is released at night
+    output = []
+    for year in range(initYear, finYear+1):
+        tempYear = []
+        if budgetSession:
+            dates = politicalCalandar[year]["budget"]
+            if skipFirstDay:
+                dates[0] += datetime.timedelta(days=1)
+            tempYear.extend(withinDates(dates[0], dates[1]))
+        if budgetEstimates:
+            dates = politicalCalandar[year]["estimates"]
+            tempYear.extend(withinDates(dates[0], dates[1]))
+        output.append(tempYear)
     return output
 
 
